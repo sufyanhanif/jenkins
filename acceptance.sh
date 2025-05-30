e66715f2ff86002228dfe20b9927f592a0f53194
sufyanhanif <hanifsufyan48@gmail.com>
2025-05-30 18:34:41 +0700

    delete

--- acceptance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Running acceptance tests..."
-if python3 app.py | grep -q "Hello World"; then
-  echo "Acceptance test passed"
-  exit 0
-else
-  echo "Acceptance test failed"
-  exit 1
-fi
